3d606ed8eb741446422602b4c482f3890a9b2c47
Jarno Rantaharju <rantahar@gmail.com>
2021-05-19 11:59:54 +0300

    Get activity zones samples

--- acceslink.py
+++ acceslink.py
@@ -144,12 +144,78 @@ def get_steps(token, user_id, url, date):
     print(r)
     r = r.json()
     with open(f"activity_steps_{user_id}_{activity}.csv", 'w') as csv_file:
-        csv_file.write(f"time, steps\n")
-        print(f"time, steps")
+        csv_file.write(f"date,time,steps\n")
+        print(f"date, time, steps")
         for s in r['samples']:
             if 'steps' in s:
                 print(s)
-                csv_file.write(f"{date}, {s['time']}, {s['steps']}\n")
+                csv_file.write(f"{date},{s['time']},{s['steps']}\n")
+
+    print(r)
+
+
+def extract_time(time_string):
+    ''' Utility for extracting hours, minutes and seconds
+    from the API time format
+
+    time_string : time value returened by the Acceslink API
+
+    return : tuple of hours, minutes and seconds
+    '''
+
+    t = ''
+    hours = 0
+    minutes = 0
+    seconds = 0
+    for c in time_string:
+        if c == 'P':
+            # The string starts with PT, which we skip
+            pass
+        elif c == 'T':
+            # The string starts with PT, which we skip
+            pass
+        elif c == 'H':
+            hours = int(t)
+            t = ''
+        elif c == 'M':
+            minutes = int(t)
+            t = ''
+        elif c == 'S':
+            seconds = int(t)
+            t = ''
+        else:
+            t += c
+
+    return (hours, minutes, seconds)
+
+
+def get_zones(token, user_id, url, date):
+
+    # The url has a constant format. Split to find activity id
+    activity = url.split('/')[-1]
+    print(activity)
+
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+
+    r = requests.get(url+'/zone-samples', headers=headers)
+    r.raise_for_status()
+    print(r)
+    r = r.json()
+    with open(f"activity_zones_{user_id}_{activity}.csv", 'w') as csv_file:
+        csv_file.write(f"date,time,zone,duration hours, duration minutes,duration seconds\n")
+        print(f"time, zones")
+        for s in r['samples']:
+            if 'activity-zones' in s:
+                print(s)
+                time = s['time']
+                for zone in s['activity-zones']:
+                    inzone = zone['inzone']
+                    hours, minutes, seconds = extract_time(inzone)
+
+                    csv_file.write(f"{date},{time},{zone['index']},{hours},{minutes},{seconds} \n")
 
     print(r)
 
