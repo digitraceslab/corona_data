02793e51402d32670e226ac250bcb12fcf12ae45
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2023-03-20 16:06:04 +0200

    Check new users before deleting. Use the data folder

--- get_data.sh
+++ get_data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-python3 check_new_subjects.py >> logs
 python3 remove_deleted_subjects.py >> logs
+python3 check_new_subjects.py >> logs
 python3 acceslink.py >> logs
 ./get_ids_with_data.sh > ids_with_data 
 
 echo done at $(date) >> logs
+
+#cp *.csv ~/raw_data/
 
